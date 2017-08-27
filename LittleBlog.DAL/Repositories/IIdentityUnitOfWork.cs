270e6025f32ba4faee1d5c9fcbfb78c6f14da773
Maxim Suprunenko <suprmaks@gmail.com>
2017-08-27 18:03:44 +0300

    Add unit of work, Project can be branch to social network

---
+++ LittleBlog.DAL/Repositories/IIdentityUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using LittleBlog.DAL.Identity;
+using LittleBlog.DAL.Persistence;
+
+namespace LittleBlog.DAL.Repositories
+{
+    public interface IIdentityUnitOfWork : IUnitOfWork
+    {
+        AppUserManager UserManager { get; }
+        AppRoleManager RoleManager { get; }
+    }
+}
